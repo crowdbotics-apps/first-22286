9357adcdefe2c8854e03a0a38c454b381ff96405
Crowdbotics <team@crowdbotics.com>
2020-11-04 09:45:14 +0000

    add package blueprint-react-native-maps-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Maps167465', human_name: 'Maps', access_route: 'Maps167465', icon: 'map'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
